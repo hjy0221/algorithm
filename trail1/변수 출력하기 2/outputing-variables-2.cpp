5408c3934e28d16bb7f3683b3d05146da48c82e2
Jae-Yoon Ha <ds970221@gmail.com>
2026-09-08 16:39:17 +0900

    [Passed] 변수 출력하기 2 (2ms, 0MB)

---
+++ trail1/변수 출력하기 2/outputing-variables-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a = 3;
+    char c = 'C';
+
+    cout << c << "!.....!" << a;
+    return 0;
+}
